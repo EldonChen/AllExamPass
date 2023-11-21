b6a69d47529b441f9ea0aad7f7b0fdaf1b8e96dc
Eldon Chen <760544706@qq.com>
2023-11-22 02:20:19 +0800

    [feature]添加了题干显示的逻辑，在题干、标签和题目之间添加了间距

--- src/Components/Questions/QuestionAreaCard.tsx
+++ src/Components/Questions/QuestionAreaCard.tsx
@@ -1,16 +1,20 @@
-import React, {useContext} from "react";
-import {Tag} from "antd";
+import React from "react";
+import {Space, Tag} from "antd";
 import Question from "@/Entries/QuestionEntries/Question";
 import QuestionContent from "@/Entries/QuestionEntries/QuestionContent";
 import QUESTION_TYPE from "@/Entries/Code/QUESTION_TYPE";
 import QuestionSingleChoice from "@/Components/Questions/QuestionSingleChoice";
-import {ExamContext} from "@/Context/ExamContext";
+import QuestionMultipleChoice from "@/Components/Questions/QuestionMultipleChoice";
 
 
 const getQuestionContent = (questionContent:QuestionContent): React.JSX.Element => {
   if(questionContent.questionTypeId === QUESTION_TYPE.SINGLE_CHOICE){
     // @ts-ignore
     return <QuestionSingleChoice>{questionContent}</QuestionSingleChoice>
+  }
+  else if(questionContent.questionTypeId === QUESTION_TYPE.MULTIPLE_CHOICE){
+    // @ts-ignore
+    return <QuestionMultipleChoice>{questionContent}</QuestionMultipleChoice>
   }
   else{
     return <></>
@@ -22,12 +26,15 @@ const QuestionAreaCard = ({children}: {children: Question[]}):React.JSX.Element=
   let questionCardContent = children.map((question, index) => {
     let questionContent = getQuestionContent(question.questionContent);
     return (
-      <div key={index}>
+      <Space key={index} direction="vertical">
         <div>
           <b>{question.questionIndex}</b>. <Tag>{question.questionContent.questionTypeName}</Tag>(本题{question.questionPointValue}分)
         </div>
+        <div>
+          {question.questionStem}
+        </div>
         <div>{questionContent}</div>
-      </div>
+      </Space>
     )
   })
   return (
